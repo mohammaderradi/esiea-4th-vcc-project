90d8021caa9928a234072f282bfaab848ff95544
mohammaderradi <121487076+mohammaderradi@users.noreply.github.com>
2023-01-08 15:29:29 +0100

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,12 +1,10 @@
-
-
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=admin
 export POSTGRES_PASSWORD=admin1234
-export POSTGRES_HOST=udagramd-db
+export POSTGRES_HOST=localhost
 export POSTGRES_DB=udagramdb
-export AWS_BUCKET=udagram-esiea-8765
+export AWS_BUCKET=arn:aws:s3:::udagram-instagram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
